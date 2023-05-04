8c91b931628faf2e63178a8aefa8b4f9114cbcff
Peter Pham <peterpham68@gmail.com>
2023-05-04 13:46:53 -0400

    Add navbar

--- Notex/Notex/ViewController.swift
+++ Notex/Notex/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var canvasView: PKCanvasView!
+    @IBOutlet weak var pencilFingerButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveToCameraRoll(_ sender: Any) {
+    }
+    
 }
 
