46ed12ea076870a8dbf53bbd25cb4706e76c1b4f
Peter Pham <peterpham68@gmail.com>
2023-05-04 12:57:58 -0400

    Init Xcode Project

---
+++ Notex/Notex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notex
+//
+//  Created by Peter Pham on 2023-05-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
