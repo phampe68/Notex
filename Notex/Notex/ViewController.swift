c134c564dab808091008ee73f863a67b05fd937a
Peter Pham <peterpham68@gmail.com>
2023-05-04 16:31:12 -0400

    Adds Pencil Tool Kit

--- Notex/Notex/ViewController.swift
+++ Notex/Notex/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import PencilKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
+    var toolPicker: PKToolPicker!
 
     @IBOutlet weak var canvasView: PKCanvasView!
     @IBOutlet weak var pencilFingerButton: UIBarButtonItem!
+    
+    // standard iPad portrait width
+    let canvasWidth: CGFloat = 768
+    let canvasOverscrollHeight: CGFloat = 500
+
+    // for storing drawings
+    var drawing = PKDrawing()
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // init canvasView with view controller
+        canvasView.delegate = self
+        canvasView.drawing = drawing
+        canvasView.alwaysBounceVertical = true
+        canvasView.drawingPolicy = PKCanvasViewDrawingPolicy.anyInput // let finger
+        
+        // Set up the tool picker
+        if #available(iOS 14.0, *) {
+            toolPicker = PKToolPicker()
+        } else {
+            // Set up the tool picker, using the window of our parent because our view has not
+            // been added to a window yet.
+            let window = parent?.view.window
+            toolPicker = PKToolPicker.shared(for: window!)
+        }
+        
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+        toolPicker.addObserver(self)
+        canvasView.becomeFirstResponder()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
     @IBAction func saveToCameraRoll(_ sender: Any) {
